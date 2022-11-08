8e032c5ab30f3d8153abc8753908e9be0ca0089f
Shubham Ranjan <shubham.ranjan@ericsson.com>
2022-11-08 13:36:27 +0530

    feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div>
+      <Header />
     </div>
+    
   )
 }
 
---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../App.css'
+
+function Header() {
+
+  return (
+    <header className="header">
+      <h2 className="product">Demo App</h2>
+      <span className="user">Shubham Ranjan</span>
+    </header>
+  )
+}
+
+export default Header
